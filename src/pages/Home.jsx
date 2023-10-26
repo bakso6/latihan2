901a3691d90c88dc4be54d924bf3c37729b6d323
bakso6 <gusbahfk@gmail.com>
2023-10-26 10:28:33 +0700

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from 'C:/VS CODE/PEMROGRAMAN WEB/latihan2//src/components/Hero.jsx';
+
+function Home() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default Home;
